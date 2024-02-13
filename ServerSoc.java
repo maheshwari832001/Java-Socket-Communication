3baa078a4a52e5c384528bf222d46565ee4a5e9a
maheshwari832001 <129958547+maheshwari832001@users.noreply.github.com>
2024-02-13 16:17:08 +0530

    Update ServerSoc.java

--- ServerSoc.java
+++ ServerSoc.java
@@ -1,16 +1,15 @@
-import java.net.Socket;
-import java.net.ServerSocket;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.BufferedReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerSoc {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         System.out.println("Server is started");
         ServerSocket ss = new ServerSocket(9999);
-          
+
         System.out.println("Server is waiting for client request");
         Socket s = ss.accept();
 
@@ -21,10 +20,15 @@ public class ServerSoc {
 
         System.out.println("Client data " + str);
 
+        //from server to client
+        String respond = str.substring(0, 6);
         OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
         PrintWriter out = new PrintWriter(os);
-        out.print(str);
-        os.flush();        
+        out.print(respond);
+        out.flush();
+        System.out.println("S: data sent from server to client");
 
+        s.close();
+        ss.close();
     }
 }
