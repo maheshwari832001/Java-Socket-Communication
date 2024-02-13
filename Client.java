0f6ba8727aad48f31958f45a96ca56035677f5c7
maheshwari832001 <129958547+maheshwari832001@users.noreply.github.com>
2024-02-13 16:15:59 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,20 +1,25 @@
 import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 
 public class Client {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         String ip = "localhost";
         int port = 9999; // 0-1023 to 65535
-        Socket s = new Socket(ip,port);
+        Socket s = new Socket(ip, port);
 
         String str = "mahe Babu";
 
         OutputStreamWriter os = new OutputStreamWriter(s.getOutputStream());
         PrintWriter out = new PrintWriter(os);
-        out.print(str);
+        out.println(str);
         os.flush();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        String respond = br.readLine();
+        System.out.println("Server response: " + respond);
 
         s.close();
     }
